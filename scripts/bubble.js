950acfdf0a8c97f07c8fe09ff2f19d6fd23b7da8
Whitneyas <whitneyserwaa@gmail.com>
2022-12-01 15:24:57 +0100

    updated

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -6,31 +6,29 @@ let theData;
 let dataSet = d3.json('../disney.json')
   .then(d => dataSet = d)
   .then(d => {
-    theData = d.filter(item => item.release_year >= "2010");
+    theData = d.filter(item => item.release_year >= "2021");
     drawChart(theData)
   })
   .catch(e => console.log("dataophalenmislukt"));
 
+  
 // de layout van de bubble creeeren
 function drawChart(dataSet) {
-  var width = window.innerWidth,height = 500
+var width = window.innerWidth,height = 550
   var simulation = d3.forceSimulation(dataSet)
-    .force('charge', d3.forceManyBody().strength(5))
-    .force('center', d3.forceCenter(width / 2, height / 2))
-    .force('collision', d3.forceCollide().radius(22))
-    .on('tick', ticked);
+      .force('charge', d3.forceManyBody().strength(10))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .force('collision', d3.forceCollide().radius(22))
+      .on('tick', ticked);
 
     d3.select('svg')
-    .attr('width', window.innerWidth)
-    .attr('height', 500)
-    .selectAll('circle')
-    .data(dataSet)
-    .join('circle')
-    .attr('r', 20)
-    .style('fill', '#00FFFF')
-    .style('stroke', '#00FFFF')
-    // .attr('cx', d => d.x)
-    // .attr('cy', d => d.y)
+      .attr('width', window.innerWidth)
+      .attr('height', 500)
+      .selectAll('circle')
+      .data(dataSet)
+      .join('circle')
+      .attr('r', 20)
+      .style('fill', '#00FFFF')
 
     .on("mouseover touchstart", (e, d) =>
       d3
@@ -38,9 +36,9 @@ function drawChart(dataSet) {
       .transition()
       .duration(175)
       .style("opacity", 1)
-      .text(`${d.title} 
-      ${d.description}
-      ${d.type}`) 
+      .text(`${d.title}${d.description}${d.type}`)  
+      .attr('cx', d => d.x)
+      .attr('cy', d => d.y)
     )
 
     // Positie van de tooltip
@@ -50,8 +48,8 @@ function drawChart(dataSet) {
       .style("left", e.pageX + 15 + "px")
       .style("top", e.pageY + 15 + "px")
     )
-    // Als de muis buiten de cirkel komt, verberg ik de tooltip
 
+    // Als de muis buiten de cirkel komt, verberg ik de tooltip
     .on("mouseout", e => d3.select(".tooltip").style("opacity", 0));
     
     //Als je ergens buiten de cirkel tapt , verberg ik de tooltip
@@ -60,80 +58,27 @@ function drawChart(dataSet) {
 
 }
 
-
-
-// drawChart(selectie);
-
 // Groepje cirkels maken
 function ticked() {
   d3.select('svg')
-    // .attr('width', window.innerWidth)
-    // .attr('height', 500)
     .selectAll('circle')
-    // .data(dataSet)
-    // .join('circle')
-    // .attr('r', 20)
-    // .style('fill', '#00FFFF')
-    // .style('stroke', '#00FFFF')
     .attr('cx', d => d.x)
     .attr('cy', d => d.y)
-
-  // //Text binnen de cirkels plaatsen
-  // d3.select('svg')
-  //   .selectAll('text')
-  //   .data(dataSet)
-  //   .join('text')
-  //   .text(d => d.title)
-  //   .attr('fill', 'black')
-  //   .attr('font-size', '4px')
-  //   .attr('text-anchor', 'middle')
-  //   .attr('x', d => d.x)
-  //   .attr('y', d => d.y)
-   
-
-    //Hoverstate 
-    // .on("mouseover touchstart", (e, d) =>
-    //   d3
-    //   .select(".tooltip")
-    //   .transition()
-    //   .duration(175)
-    //   .style("opacity", 1)
-    //   .text(`${d.title} 
-    //   ${d.description}
-    //   ${d.type}`) 
-    // )
-
-    // // Positie van de tooltip
-    // .on("mousemove", (e) =>
-    //   d3
-    //   .select(".tooltip")
-    //   .style("left", e.pageX + 15 + "px")
-    //   .style("top", e.pageY + 15 + "px")
-    // )
-    // Als de muis buiten de cirkel komt, verberg ik de tooltip
-
-//     .on("mouseout", e => d3.select(".tooltip").style("opacity", 0));
-    
-//     //Als je ergens buiten de cirkel tapt , verberg ik de tooltip
-//     d3.select("body").on("touchend", e =>
-//     d3.select("#tooltip").style("opacity", 0));
-
 }
 
 function applyFilter(event) {
-  console.log(event.target.value)
+  // console.log(event.target.value)
  let range,newData;
 
   if(event.target.value !== 'all') {
     // console.log(theData)
     newData = theData.filter(film => {
-      // return film.listed_in === Musical(range[0]) && film.listed_in <= Number(range[1])
       return film.listed_in.includes(event.target.value)
     })
     drawChart(newData);
     // ticked(newData)
 
-    console.log(newData);
+    // console.log(newData);
   } else {
     drawChart(theData)
     // ticked(theData)
